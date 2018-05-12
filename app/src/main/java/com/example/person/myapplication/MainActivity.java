ab33c2181282803a8d2608159f374e85e7b38fd6
HyeonMooJeon <dnfmanage@naver.com>
2018-05-12 18:06:23 +0900

    first

---
+++ app/src/main/java/com/example/person/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.person.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    static void main(String args[]){
+        System.out.print("차오니마!");   System.out.print("차오니마!");   System.out.print("차오니마!");   System.out.print("차오니마!");
+    }
+}
